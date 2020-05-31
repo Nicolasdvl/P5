89fdf6e32649b2e2b6ba3b0d0fcc6c600eba4838
Daval Nicolas <nicolasdvl@vivaldi.net>
2020-06-01 00:43:57 +0200

    improve clean

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from models import data, stores, categories
 from config import uri
 from sqlalchemy import *
 from collect import collect
-from clean import clean_for_data
+from clean import clean_for_data, clean_for_cat, clean_for_store
 
 engine = create_engine(uri)
 
@@ -14,3 +14,7 @@ stores.Base.metadata.create_all(engine)
 request = 'https://fr.openfoodfacts.org/cgi/search.pl?action=process&page_size=5&json=true'
 products = collect(request)
 table_data = clean_for_data(products)
+table_cat = clean_for_cat(products)
+table_store = clean_for_store(products)
+
+print(table_cat)
--- clean.py
+++ clean.py
@@ -5,9 +5,18 @@ def clean_for_data(list):
         table.append((list[pos]["code"], list[pos]["product_name"], list[pos]["ingredients_text_fr"], list[pos]["labels"], list[pos]["nutriscore_grade"]))
     return table   
 
-def clean_for_categories(list):
+def clean_for_cat(list):
 
     table = []
     for pos, element in enumerate(list):
-        table.append()
-    return table  
+        for pos_2, element_2 in enumerate(list[pos]["categories_tags"]):
+            table.append(list[pos]["categories_tags"][pos_2])
+    return table 
+
+def clean_for_store(list):
+
+    table =[]
+    for pos, element in enumerate(list):
+        for pos_2, element_2 in enumerate(list[pos]["stores_tags"]):
+            table.append(list[pos]["stores_tags"][pos_2])
+    return table
